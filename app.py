792ba41879a863a279dbf215df303c2508256a65
varanthornsak <varanthorn.s.ysda@gmail.com>
2026-02-20 13:59:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import datetime
 import time
+import uuid
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import inch
